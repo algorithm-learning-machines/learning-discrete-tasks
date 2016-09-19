f8cc3fbb96678bee0b5a68391c7f66e4dc937f80
Zappaman <iosifadrianmihai@gmail.com>
2016-09-20 02:48:37 +0300

    safety commit

--- main.lua
+++ main.lua
@@ -8,41 +8,6 @@ require("rnn")
 require("tasks.doom_clock")
 require("tasks.all_tasks")
 
-
-function print_r ( t )  
-   local print_r_cache={}
-   local function sub_print_r(t,indent)
-      if (print_r_cache[tostring(t)]) then
-         print(indent.."*"..tostring(t))
-      else
-         print_r_cache[tostring(t)]=true
-         if (type(t)=="table") then
-            for pos,val in pairs(t) do
-               if (type(val)=="table") then
-                  print(indent.."["..pos.."] => "..tostring(t).." {")
-                  sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                  print(indent..string.rep(" ",string.len(pos)+6).."}")
-               elseif (type(val)=="string") then
-                  print(indent.."["..pos..'] => "'..val..'"')
-               else
-                  print(indent.."["..pos.."] => "..tostring(val))
-               end
-            end
-         else
-            print(indent..tostring(t))
-         end
-      end
-   end
-   if (type(t)=="table") then
-      print(tostring(t).." {")
-      sub_print_r(t,"  ")
-      print("}")
-   else
-      sub_print_r(t,"  ")
-   end
-end
-
-
 local opts = {}
 opts.fixedLength = true
 
@@ -50,8 +15,14 @@ local tasks = allTasks()
 --print(tasks)
 
 for k,v in ipairs(tasks) do
-   local T = getTask(v) 
-   local t = T()
+   local t = getTask(v)() 
+   local X, T, F, L = t:updateBatch()
+
+   local inputSize, outputSize
+  
+
+   local seqLSTM = nn.Sequencer(nn.FastLSTM(t.inputSize, t.outputSize, 
+      backSteps))
 end
 
 --local dc = DoomClock()
