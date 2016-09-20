2bf52c52af453bc243a03b36720dd26962318898
Tudor Berariu <tudor.berariu@gmail.com>
2016-09-20 16:49:41 +0300

    Added backward phase through spllitter

--- playground/tensor_to_table.lua
+++ playground/tensor_to_table.lua
@@ -9,21 +9,55 @@ print(x)
 print("------------------------------")
 
 -- Create model that splits in a table
-unflattener = nn.ConcatTable()
+splitter = nn.ConcatTable()
 
-unflattener:add(nn.Narrow(3, 1, 3))   -- 1-3
-unflattener:add(nn.Narrow(3, 4, 4))   -- 4-7
-unflattener:add(nn.Narrow(3, 8, 2))   -- 8-9
+splitter:add(nn.Narrow(3, 1, 3))   -- 1-3
+splitter:add(nn.Narrow(3, 4, 4))   -- 4-7
+splitter:add(nn.Narrow(3, 8, 2))   -- 8-9
 
-print(unflattener)
+print(splitter)
 
 print("------------------------------")
 
 -- Split the tensor
-z = unflattener:forward(x)
+z = splitter:forward(x)
 
 print(z)
 for k, v in pairs(z) do print("Output " .. k); print(v); end
 
 print("------------------------------")
 
+-- Create some criterion and fake targets
+
+c = nn.ParallelCriterion()
+
+c:add(nn.MSECriterion())
+c:add(nn.MSECriterion())
+c:add(nn.MSECriterion())
+
+t = {}
+t[1] = torch.rand(2, 2, 3)
+t[2] = torch.rand(2, 2, 4)
+t[3] = torch.rand(2, 2, 2)
+
+-- Forward through criterion
+
+loss = c:forward(z, t)
+
+print("Loss = " .. loss)
+print("------------------------------")
+
+-- Backward through criterion
+
+dz = c:backward(z, t)
+
+print("dz (grad output as table)")
+for k, v in pairs(dz) do print("Grad output " .. k); print(v); end
+print("------------------------------")
+
+-- Backward through splitter
+
+dx = splitter:backward(x, dz)
+
+print("dx (grad output as tensor")
+print(dx)
